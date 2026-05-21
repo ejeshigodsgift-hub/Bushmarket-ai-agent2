0abd73bf9dcbe83604d369eee137966f6ef0c3a4
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-21 23:57:58 +0100

    Update event_types.py

--- events/event_types.py
+++ events/event_types.py
@@ -2,3 +2,16 @@ COOP_FUNDED = "cooperative_funded"
 BULK_ORDER_TRIGGERED = "bulk_order_triggered"
 DELIVERY_COMPLETED = "delivery_completed"
 PARTIAL_PROCUREMENT = "partial_procurement"
+
+MEMBER_JOINED = "member_joined"
+PAYMENT_SUCCESS = "payment_success"
+PAYMENT_FAILED = "payment_failed"
+
+FUNDING_MILESTONE_REACHED = "funding_milestone_reached"
+COOPERATIVE_FUNDED = "cooperative_funded"
+
+BULK_PURCHASE_TRIGGERED = "bulk_purchase_triggered"
+
+DELIVERY_COMPLETED = "delivery_completed"
+
+REFUND_ISSUED = "refund_issued"
