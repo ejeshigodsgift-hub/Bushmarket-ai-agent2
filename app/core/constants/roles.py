86ae4b922ff5829dcc2e548bf6966b45d7a89b18
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-06-05 17:02:22 +0100

    Create constants/roles.py

---
+++ app/core/constants/roles.py
@@ -0,0 +1,13 @@
+# app/core/constants/roles.py
+
+ROLE_SHOPPER = "shopper"
+ROLE_AGENT = "agent"
+ROLE_ADMIN = "admin"
+ROLE_MEMBER = "member"
+
+ALL_ROLES = {
+    ROLE_SHOPPER,
+    ROLE_AGENT,
+    ROLE_ADMIN,
+    ROLE_MEMBER
+}
