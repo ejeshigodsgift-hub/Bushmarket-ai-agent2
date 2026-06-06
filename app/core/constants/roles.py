11c40bc3c75935ea83d791f4a3ac8dcb8f7afa4e
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-06-06 08:26:27 +0100

    Update roles.py

--- app/core/constants/roles.py
+++ app/core/constants/roles.py
@@ -3,11 +3,11 @@
 ROLE_SHOPPER = "shopper"
 ROLE_AGENT = "agent"
 ROLE_ADMIN = "admin"
-ROLE_MEMBER = "membership"
+
 
 ALL_ROLES = {
     ROLE_SHOPPER,
     ROLE_AGENT,
     ROLE_ADMIN,
-    ROLE_MEMBERSHIP
+    
 }
