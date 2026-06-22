25c548025d041c17e3faa87b3d4a90841b50f8dd
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-06-22 14:58:40 +0100

    Create app/db/seeds/system_cooperatives.py

---
+++ app/db/seeds/system_cooperatives.py
@@ -0,0 +1,9 @@
+system_wallet_coop = Cooperative(
+    id="system-wallet-coop",
+    name="System Wallet Cooperative"
+)
+
+system_marketplace_coop = Cooperative(
+    id="system-marketplace-coop",
+    name="System Marketplace Cooperative"
+)
