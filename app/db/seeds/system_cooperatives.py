891846a656406a338e65c74ca379a9b1dc716c6b
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-06-22 18:05:12 +0100

    Update system_cooperatives.py

--- app/db/seeds/system_cooperatives.py
+++ app/db/seeds/system_cooperatives.py
@@ -1,9 +1,29 @@
-system_wallet_coop = Cooperative(
-    id="system-wallet-coop",
-    name="System Wallet Cooperative"
-)
+from app.db.models.cooperative import Cooperative
 
-system_marketplace_coop = Cooperative(
-    id="system-marketplace-coop",
-    name="System Marketplace Cooperative"
-)
+WALLET_COOPERATIVE_ID = "system-wallet-coop"
+MARKETPLACE_COOPERATIVE_ID = "system-marketplace-coop"
+COOPERATIVE_COOPERATIVE_ID = "system-cooperative-coop"
+
+async def seed_system_cooperatives(db):
+    system_wallet_coop = Cooperative(
+        id=WALLET_COOPERATIVE_ID,
+        name="System Wallet Cooperative"
+    )
+
+    system_marketplace_coop = Cooperative(
+        id=MARKETPLACE_COOPERATIVE_ID,
+        name="System Marketplace Cooperative"
+    )
+
+    system_cooperative_coop = Cooperative(
+        id=COOPERATIVE_COOPERATIVE_ID,
+        name="System Cooperative Master"
+    )
+
+    db.add_all([
+        system_wallet_coop,
+        system_marketplace_coop,
+        system_cooperative_coop
+    ])
+
+    await db.commit()
