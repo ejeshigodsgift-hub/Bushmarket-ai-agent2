a4103346ff6426cc0c0d51bd71774095a9aeb2fa
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-06-24 20:06:07 +0100

    Update notification_handler.py

--- app/handlers/notification_handler.py
+++ app/handlers/notification_handler.py
@@ -1,8 +1,52 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import select
+
+from app.db.models.notification import Notification
+
+
 class NotificationHandler:
 
-async def handle(self, db, event):
-
-    print(
-        "[NOTIFICATION]",
+    async def handle(
+        self,
+        db,
         event
-    )
+    ):
+
+        topic = event["topic"]
+        payload = event["payload"]
+
+        if topic == "notification.sms.send":
+
+            # ===================================
+            # CALL SMS PROVIDER
+            # ===================================
+            sms_sent = True
+
+            # ===================================
+            # SMS SENT SUCCESSFULLY
+            # ===================================
+            if sms_sent:
+
+                stmt = (
+                    select(Notification)
+                    .where(
+                        Notification.id ==
+                        payload["notification_id"]
+                    )
+                    .limit(1)
+                )
+
+                result = await db.execute(stmt)
+
+                notification = result.scalar_one_or_none()
+
+                if notification:
+
+                    notification.status = "sent"
+
+                    notification.sent_at = (
+                        datetime.now(timezone.utc)
+                    )
+
+                    await db.commit()
