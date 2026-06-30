261f741dccc1577a609b21f60417bc926270c0b2
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-06-30 12:13:50 +0100

    Delete procurement_client.py

--- integrations/procurement_client.py
+++
@@ -1,7 +0,0 @@
-class ProcurementClient:
-
-    def trigger_bulk_purchase(self, event):
-        print(f"[PROCUREMENT] Triggered for coop {event['cooperative_id']}")
-
-    def execute_order(self, event):
-        print(f"[ORDER EXECUTION] Coop {event['cooperative_id']}")
