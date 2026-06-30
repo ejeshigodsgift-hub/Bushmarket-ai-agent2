b23286c829f52bce6d99ab374ec7242b625040d9
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-06-30 12:10:02 +0100

    Delete notification_client.py

--- integrations/notification_client.py
+++
@@ -1,7 +0,0 @@
-class NotificationClient:
-
-    def send(self, user_id, message):
-        print(f"[NOTIFY USER] {user_id}: {message}")
-
-    def broadcast(self, cooperative_id, message):
-        print(f"[BROADCAST coop={cooperative_id}] {message}")
